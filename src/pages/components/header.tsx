7b55c8f22b884d74d8a78ceefa4ee1314611007e
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-03-25 13:00:35 -0300

    fix menu hidden in sigin page in version mobil

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -29,7 +29,7 @@ export default function Header({menuType, buttonVisible}:HeaderProps) {
   ];
 
   // Filtra os itens do menu com base na prop menuType
-  const filteredMenu = menuType === "home" ? [] : menuItems;
+  const filteredMenu = menuType === "full" && menuItems;
 
   return (
     <div className="flex flex-row items-center justify-between bg-black h-16 md:pr-6 pl-6 w-screen fixed top-0 z-20">
@@ -41,7 +41,7 @@ export default function Header({menuType, buttonVisible}:HeaderProps) {
           LOGO
         </div>
         <ul className="hidden md:flex md:flex-row md:gap-8 font-light text-white">
-          {filteredMenu.map((item) => (
+          {filteredMenu && filteredMenu.map((item) => (
             <li key={item.to} className="hover:text-gray-400 cursor-pointer">
               <Link to={item.to} smooth={true} duration={1000}>
                 {item.name}
@@ -52,7 +52,7 @@ export default function Header({menuType, buttonVisible}:HeaderProps) {
       </div>
 
       {/* Botão de Login e Menu Sanduíche em telas menores */}
-      <div className="flex items-center gap-4 h-full">
+      { menuType==="full" && (<div className="flex items-center gap-4 h-full">
         <div className="md:hidden">
           <button onClick={() => setMenuOpen(!menuOpen)} className="text-white">
             {menuOpen ? <X size={28} /> : <Menu size={28} />}
@@ -66,11 +66,12 @@ export default function Header({menuType, buttonVisible}:HeaderProps) {
         </button>
         )}
       </div>
+      )}
 
       {/* Menu Sanduíche */}
       {menuOpen && (
         <ul className="absolute top-16 left-0 w-full bg-gray-300 flex flex-col items-center gap-6 py-6 font-light text-black md:hidden z-10">
-          {filteredMenu.map((item) => (
+          {filteredMenu && filteredMenu.map((item) => (
             <li
               key={item.to}
               className="w-full text-center hover:bg-gray-500 hover:text-white cursor-pointer py-2"
