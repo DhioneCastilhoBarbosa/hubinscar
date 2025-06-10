dccfe5013c4204b56677f7501aa8ca41d415deed
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-06-10 09:24:30 -0300

    feat: add api send email

--- src/pages/components/contact/contact.tsx
+++ src/pages/components/contact/contact.tsx
@@ -1,68 +1,158 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Footer from "../footer";
+import { toast } from "sonner";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: ""
+  });
+
+  const [isSending, setIsSending] = useState(false);
+  
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSending(true);
+  
+    
+
+    try {
+      await axios.post("https://mail.api-castilho.com.br/send-email", formData);
+      
+      setFormData({
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        subject: "",
+        message: ""
+      });
+      toast.success("Mensagem enviada com sucesso!");
+    } catch (err: any) {
+      toast.error("Erro ao enviar:", err);
+     
+    } finally {
+      setIsSending(false);
+    }
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-stone-950 text-black">
-      {/* Conteúdo da página */}
-      <main className="flex-grow flex flex-col items-center justify-center w-full md:pb-4 ">
+      <main className="flex-grow flex flex-col items-center justify-center w-full md:pb-4">
         <div className="flex flex-col items-center justify-center md:w-5/6 mt-20 bg-gray-200 rounded-md max-w-4xl">
           <div className="flex flex-col items-center justify-center w-full gap-6">
             <h1 className="text-gray-900 font-extrabold text-4xl mt-8">Como podemos ajudar?</h1>
             <p className="mx-4">Envie para a nossa equipe suas dúvidas, sugestões e reclamações. Estaremos prontos para te ajudar.</p>
           </div>
           <div className="flex flex-col items-center justify-center w-full gap-6 mt-8">
-            <form className="flex flex-col items-center justify-center w-full gap-4 p-6">
-              {/* Nome + Sobrenome */}
+            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center w-full gap-4 p-6">
               <div className="flex md:flex-row flex-col w-full gap-4">
                 <div className="flex flex-col md:w-1/2">
                   <label className="mb-1 text-sm font-medium text-gray-700">Nome</label>
-                  <input type="text" placeholder="Digite seu nome" className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black" />
+                  <input
+                    type="text"
+                    name="first_name"
+                    value={formData.first_name}
+                    onChange={handleChange}
+                    placeholder="Digite seu nome"
+                    required
+                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                  />
                 </div>
                 <div className="flex flex-col md:w-1/2">
                   <label className="mb-1 text-sm font-medium text-gray-700">Sobrenome</label>
-                  <input type="text" placeholder="Digite seu sobrenome" className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black" />
+                  <input
+                    type="text"
+                    name="last_name"
+                    value={formData.last_name}
+                    onChange={handleChange}
+                    placeholder="Digite seu sobrenome"
+                    required
+                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                  />
                 </div>
               </div>
 
-              {/* Email + Telefone */}
               <div className="flex md:flex-row flex-col w-full gap-4">
                 <div className="flex flex-col md:w-1/2">
                   <label className="mb-1 text-sm font-medium text-gray-700">Email</label>
-                  <input type="email" placeholder="Digite seu email" className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black" />
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite seu email"
+                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                  />
                 </div>
                 <div className="flex flex-col md:w-1/2">
                   <label className="mb-1 text-sm font-medium text-gray-700">Telefone</label>
-                  <input type="tel" placeholder="(xx)xxxx-xxxx" className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black" />
+                  <input
+                    type="tel"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    required
+                    placeholder="(xx)xxxxx-xxxx"
+                    className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                  />
                 </div>
               </div>
 
-              {/* Assunto */}
               <div className="flex flex-col w-full md:w-1/2 self-start">
                 <label className="mb-1 text-sm font-medium text-gray-700">Assunto</label>
-                <input type="text" placeholder="Digite o assunto" className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black" />
+                <input
+                  type="text"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  required
+                  placeholder="Digite o assunto"
+                  className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                />
               </div>
 
-              {/* Mensagem */}
               <div className="flex flex-col w-full">
                 <label className="mb-1 text-sm font-medium text-gray-700">Mensagem</label>
-                <textarea placeholder="Digite sua mensagem" className="p-2 border border-gray-300 rounded-md h-44 focus:outline-none focus:ring-1 focus:ring-black focus:border-black"></textarea>
+                <textarea
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                  placeholder="Digite sua mensagem"
+                  className="p-2 border border-gray-300 rounded-md h-44 focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                />
               </div>
 
-              {/* Botão */}
-              <button type="submit" className="bg-black text-white py-2 px-4 w-64 rounded-lg hover:bg-gray-700 hover:cursor-pointer">
-                Enviar
+              <button
+                type="submit"
+                className="bg-black text-white py-2 px-4 w-64 rounded-lg hover:bg-gray-700"
+                disabled={isSending}
+              >
+                {isSending ? "Enviando..." : "Enviar"}
               </button>
+
+              
             </form>
           </div>
         </div>
       </main>
-
-      {/* Footer */}
       <Footer />
     </div>
   );
--- src/pages/components/dashboard/components/support/supportPage.tsx
+++ src/pages/components/dashboard/components/support/supportPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import axios from "axios";
 import { MessageCircleQuestion, Mail, User, Phone } from "lucide-react";
 import api from "../../../../../services/api";
 
@@ -12,6 +12,8 @@ export default function Support() {
   });
 
   const [mensagemSucesso, setMensagemSucesso] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const [erro, setErro] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -19,27 +21,43 @@ export default function Support() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setIsSending(true);
+    setErro("");
+    setMensagemSucesso("");
 
-    console.log("Mensagem enviada:", formData);
-    setMensagemSucesso("Mensagem enviada com sucesso! Entraremos em contato em breve.");
-    setFormData({ nome: "", email: "", telefone: "", mensagem: "" });
+    const nomeSeparado = formData.nome.trim().split(" ");
+    const first_name = nomeSeparado[0];
+    const last_name = nomeSeparado.slice(1).join(" ") || "";
+
+    const payload = {
+      first_name,
+      last_name,
+      email: formData.email,
+      phone: formData.telefone,
+      subject: "Suporte",
+      message: formData.mensagem,
+    };
+
+    try {
+      await axios.post("https://mail.api-castilho.com.br/send-email-support", payload);
+      setMensagemSucesso("Mensagem enviada com sucesso! Entraremos em contato em breve.");
+      setFormData({ nome: "", email: "", telefone: "", mensagem: "" });
+    } catch (error) {
+      console.error("Erro ao enviar mensagem:", error);
+      setErro("Erro ao enviar a mensagem. Tente novamente mais tarde.");
+    } finally {
+      setIsSending(false);
+    }
   };
-  
 
   useEffect(() => {
     const fetchUserInfo = async () => {
       const userId = localStorage.getItem("ID");
-      
       if (!userId) return;
 
       try {
-        const response = await api.get(
-          `/user/list?id=${userId}`
-        );
-        
-
+        const response = await api.get(`/user/list?id=${userId}`);
         const user = response.data[0];
-        console.log("Dados do usuário:", user);
         if (user && user.username && user.email) {
           setFormData((prev) => ({
             ...prev,
@@ -48,7 +66,6 @@ export default function Support() {
             telefone: user.phone || "",
           }));
         }
-        
       } catch (error) {
         console.error("Erro ao buscar dados do usuário:", error);
       }
@@ -56,7 +73,7 @@ export default function Support() {
 
     fetchUserInfo();
   }, []);
-  
+
   return (
     <div className="bg-zinc-900 text-white rounded-xl p-6 shadow-lg max-w-3xl mx-auto mt-10 space-y-6 border border-zinc-700">
       <h2 className="text-2xl font-bold flex items-center gap-2">
@@ -70,6 +87,9 @@ export default function Support() {
 
       {mensagemSucesso && (
         <div className="bg-green-600 text-white p-3 rounded-lg text-sm">{mensagemSucesso}</div>
+      )}
+      {erro && (
+        <div className="bg-red-600 text-white p-3 rounded-lg text-sm">{erro}</div>
       )}
 
       <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
@@ -133,8 +153,9 @@ export default function Support() {
         <button
           type="submit"
           className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition"
+          disabled={isSending}
         >
-          Enviar solicitação
+          {isSending ? "Enviando..." : "Enviar solicitação"}
         </button>
       </form>
     </div>
